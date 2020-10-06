345b4b84a4a29eb60fec4832fe0d5e03a6901bc2
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-07 01:01:22 +0200

    Get the possible children(games) of a board at the current state

--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -1,7 +1,7 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
-  attr_reader :board, :next_mover_mark
+  attr_reader :board, :next_mover_mark, :prev_move_pos
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
     @next_mover_mark = next_mover_mark
@@ -17,5 +17,17 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    children_nodes = []
+    (3).times do |i|
+      (3).times do |j|
+        if @board[[i, j]].nil?
+          temp_board = @board.dup
+          temp_board[[i, j]] = next_mover_mark
+          next_mover_mark = temp_board[[i, j]] == :x ? :o : :x
+          children_nodes << TicTacToeNode.new(temp_board, next_mover_mark, [i, j])
+        end
+      end
+    end
+    children_nodes
   end
 end
