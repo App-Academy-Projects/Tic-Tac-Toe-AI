bd91ddfe35d8c1cfe4991ea57ed5764fd94e382e
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-07 14:49:34 +0200

    blocks an opponent's winning move

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -4,6 +4,8 @@ class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
     t_t_t = TicTacToeNode.new(game.board, mark)
     t_t_t.children.any? { |child| return child.prev_move_pos if child.winning_node?(mark) }
+
+    t_t_t.children.any? { |child| return child.prev_move_pos unless child.losing_node?(mark) }
   end
 end
 
