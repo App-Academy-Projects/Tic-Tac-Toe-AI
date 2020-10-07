b64e54ec04e897f5444c4e2bc93ae278cdcf9369
usamaeltmsah <usamaeltmsah@gmail.com>
2020-10-07 15:33:44 +0200

    Shuffle te childrens, and raise an error if bot will lose

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -3,10 +3,12 @@ require_relative 'tic_tac_toe_node'
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
     t_t_t = TicTacToeNode.new(game.board, mark)
-    children = t_t_t.children
+    children = t_t_t.children.shuffle
     children.any? { |child| return child.prev_move_pos if child.winning_node?(mark) }
 
     children.any? { |child| return child.prev_move_pos unless child.losing_node?(mark) }
+
+    raise "HAHA I'll not let you win!!"
   end
 end
 
